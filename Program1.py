6cafd188330a0f3b4afc3c7c921b2875bd70e11c
Robert McLemore <137344830+tacmed1@users.noreply.github.com>
2025-10-29 18:24:56 -0600

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,13 @@
+
+#Name: Robert McLemore
+#Assignment: Extra Credit +1
+
+
+print("  @..@ ")
+print(" (----) ")
+print("( >__< ) ")
+print(" ^^~~^^ ")
+
+state = input("US state: ")
+capital = input("Capital: ")
+print("The capital of " + state + " is " + capital)
